7d996350f6eadcf70dcae1414d7ba9fe17e3c55a
yamachu <yamachu.dev@gmail.com>
2026-01-17 00:19:24 +0900

    community nodes must have one or more nodes...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,6 @@ export default [
 	...config,
 	{
 		rules: {
-			// This package provides only credentials, so disabling the rule about empty package.json nodes array
-			'n8n-nodes-base/community-package-json-n8n-nodes-empty': 'off',
 			// Disabling icon validation rule as this package has no icon
 			'@n8n/community-nodes/icon-validation': 'off',
 			// Disabling credential test requirement as this package's credential cannot be tested automatically
